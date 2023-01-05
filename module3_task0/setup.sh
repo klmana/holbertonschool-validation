0fcbc2c5ec9c034cceec914eb494d15cc70ff2a6
klmana <km@rtin.mobi>
2023-01-05 01:00:43 +0000

    module3 task0

---
+++ module3_task0/setup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+apt-get update && apt-get install -y hugo make
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+apt install ./hugo.deb
+rm hugo.deb
+make build
