017543a3691360314024687093e9052a27d9e8e1
klmana <104742971+klmana@users.noreply.github.com>
2023-01-07 22:25:36 +1000

    lint

--- module3_task2/setup.sh
+++ module3_task2/setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo apt-get update && apt-get install -y hugo make shellcheck
+sudo apt-get update && apt-get install -y hugo make shellcheck && npm install -g markdownlint-cli
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
 sudo apt install ./hugo.deb
 sudo rm hugo.deb
