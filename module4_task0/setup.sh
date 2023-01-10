a2b9d4abc48cb38f040e11faadce5a721c633523
klmana <104742971+klmana@users.noreply.github.com>
2023-01-10 21:23:18 +1000

    update docker

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,6 +1,2 @@
 #!/bin/bash
-sudo apt-get update && apt-get install -y hugo make shellcheck
-curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
-sudo apt install ./hugo.deb
-sudo rm hugo.deb
-make build
+docker run -v "$(pwd)":/usr/src/app -w /usr/src/app golang:1.15.8-buster make build
