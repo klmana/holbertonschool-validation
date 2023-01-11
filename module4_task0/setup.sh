4e2a148816f7635630877431eef435659831e479
klmana <104742971+klmana@users.noreply.github.com>
2023-01-11 12:10:57 +1000

    remove go

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-apt-get update && apt-get install -y hugo make
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+sudo apt install ./hugo.deb
+npm install -g markdownlint-cli
+rm hugo.deb
+sudo rm -rf /usr/bin/go
