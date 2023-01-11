734afdc8e7287987df87fa513fcb19804bb9b535
klmana <104742971+klmana@users.noreply.github.com>
2023-01-11 10:52:23 +1000

    update setup

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-apt-get update && apt-get install -y make
+apt-get update && apt-get install -y shellcheck snapd git make wget build-essential && wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb && dpkg -i hugo_extended_0.109.0_linux-amd64.deb && npm install -g markdownlint-cli
