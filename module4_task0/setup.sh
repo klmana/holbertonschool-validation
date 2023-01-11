6e10e5b069480ba62d8349df3325d579359dafe0
klmana <104742971+klmana@users.noreply.github.com>
2023-01-11 12:35:19 +1000

    upgrade

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-apt-get update && apt-get install -y hugo make
-make build
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
+apt install ./hugo.deb
+npm install -g markdownlint-cli
+rm hugo.deb
