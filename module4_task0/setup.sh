3361cd13f6626b69f76e08b49e532d7e03e7f51c
klmana <104742971+klmana@users.noreply.github.com>
2023-01-11 12:17:44 +1000

    testing new set

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-apt-get update && apt-get install -y shellcheck snapd git make wget build-essential && wget https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb && dpkg -i hugo_extended_0.109.0_linux-amd64.deb && npm install -g markdownlint-cli
-sudo rm -rf /usr/bin/go
+apt-get update && apt-get install -y hugo make
+make build
+if [[ $? -ne 0 ]] ; then
+        exit 255
+fi
