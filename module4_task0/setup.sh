e13214e0a5dcbd9477c2aa2180bb2842b4e21179
klmana <104742971+klmana@users.noreply.github.com>
2023-01-12 19:19:00 +1000

    revert

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
-curl -Lo install_hugo.deb
-https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb
+curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
 sudo apt install ./hugo.deb
 npm install -g markdownlint-cli
 rm hugo.deb
