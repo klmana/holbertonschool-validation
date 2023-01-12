b860c22578d8e1c6d0536a53bae268e87e5c2e71
klmana <104742971+klmana@users.noreply.github.com>
2023-01-12 19:11:24 +1000

    removed golang

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env bash
-apt-get update && apt-get install -y golang
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
 sudo apt install ./hugo.deb
 npm install -g markdownlint-cli
