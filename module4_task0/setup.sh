55ecca07c3e49e0ba179e04a5e23d7d83b921c0f
klmana <104742971+klmana@users.noreply.github.com>
2023-01-12 15:08:31 +1000

    update again

--- module4_task0/setup.sh
+++ module4_task0/setup.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-if [[ $? -ne 0 ]] ; then
-        exit 255
-fi
 curl -L https://github.com/gohugoio/hugo/releases/download/v0.109.0/hugo_extended_0.109.0_linux-amd64.deb -o hugo.deb
 sudo apt install ./hugo.deb
 npm install -g markdownlint-cli
