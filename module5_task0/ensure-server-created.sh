eba7df646e7c5c70543e81685b0c6e749975d47b
klmana <104742971+klmana@users.noreply.github.com>
2023-01-20 06:24:27 +1000

    add doble quotes

--- module5_task0/ensure-server-created.sh
+++ module5_task0/ensure-server-created.sh
@@ -13,9 +13,9 @@ else
 
   # create the security group
   sg_id=$(aws ec2 create-security-group --group-name awesome-sg --description "Security group for awesome server" --query 'GroupId' --output text)
-  aws ec2 authorize-security-group-ingress --group-id $sg_id --protocol tcp --port 22 --cidr 0.0.0.0/0
-  aws ec2 authorize-security-group-ingress --group-id $sg_id --protocol tcp --port 80 --cidr 0.0.0.0/0
-  aws ec2 authorize-security-group-ingress --group-id $sg_id --protocol tcp --port 443 --cidr 0.0.0.0/0
+  aws ec2 authorize-security-group-ingress --group-id "$sg_id" --protocol tcp --port 22 --cidr 0.0.0.0/0
+  aws ec2 authorize-security-group-ingress --group-id "$sg_id" --protocol tcp --port 80 --cidr 0.0.0.0/0
+  aws ec2 authorize-security-group-ingress --group-id "$sg_id" --protocol tcp --port 443 --cidr 0.0.0.0/0
 
   # create the instance
   instance_id=$(aws ec2 run-instances --image-id ami-0ac019f4fcb7cb7e6 --instance-type t3.micro --key-name awesome-key --security-group-ids $sg_id --tag-specifications 'ResourceType=instance,Tags=[{Key=Name,Value=awesome-server}]' --query 'Instances[0].InstanceId' --output text)
