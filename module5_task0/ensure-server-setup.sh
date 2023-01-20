e139a50300ed1e98ce8ff1db618d0dda29640a1a
klmana <104742971+klmana@users.noreply.github.com>
2023-01-20 15:41:44 +1000

    authorize an ingress rule

--- module5_task0/ensure-server-setup.sh
+++ module5_task0/ensure-server-setup.sh
@@ -12,10 +12,10 @@ else
 fi
 
 # check if the ingress rule already exists
-rule_exists=$(aws ec2 describe-security-groups --group-ids "$sg_id" --query "SecurityGroups[].IpPermissions[?FromPort=='22'].IpRanges[?CidrIp=='0.0.0.0/0']" --output text)
-if [ -z "$rule_exists" ]; then
-  # authorize security group ingress
-  aws ec2 authorize-security-group-ingress --group-id "$sg_id" --protocol tcp --port 22 --cidr 0.0.0.0/0
+existing_rule=$(aws ec2 describe-security-groups --group-ids "$sg_id" --query "SecurityGroups[].IpPermissions[?FromPort=='22'].IpRanges[?CidrIp=='0.0.0.0/0']" --output text)
+if [ -z "$existing_rule" ]; then
+    # authorize security group ingress
+    aws ec2 authorize-security-group-ingress --group-id "$sg_id" --protocol tcp --port 22 --cidr 0.0.0.0/0
 else
-  echo "Ingress rule already exists, skipping authorize step"
+    echo "Ingress rule already exists, skipping authorize step"
 fi
