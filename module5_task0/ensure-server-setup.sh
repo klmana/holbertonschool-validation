fa60eeeb1bca395a82727efba2048624ceb03317
klmana <104742971+klmana@users.noreply.github.com>
2023-01-20 15:14:29 +1000

    update ensure-server-setup.sh

--- module5_task0/ensure-server-setup.sh
+++ module5_task0/ensure-server-setup.sh
@@ -1,15 +1,13 @@
-#!/bin/bash
-
-# check if instance already exists
-instance_id=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=awesome-server" --query "Reservations[].Instances[].InstanceId" --output text)
-
-if [ -n "$instance_id" ]; then
-    # get public dns name of existing instance
-    dns=$(aws ec2 describe-instances --instance-ids "$instance_id" --query "Reservations[].Instances[].PublicDnsName" --output text)
-    echo "Instance already exists with public DNS name: $dns"
+# check if security group already exists
+existing_sg=$(aws ec2 describe-security-groups --filters "Name=group-name,Values=awesome-sg" --query "SecurityGroups[].GroupId" --output text)
+if [ -n "$existing_sg" ]; then
+    echo "Security group 'awesome-sg' already exists with id: $existing_sg"
+    sg_id=$existing_sg
 else
     # create security group
     sg_id=$(aws ec2 create-security-group --group-name awesome-sg --description "Security group for awesome server" --query 'GroupId' --output text)
-    aws ec2 authorize-security-group-ingress --group-id "$sg_id" --protocol tcp --port 22 --cidr 0.0.0.0/0
-    aws ec2 authorize-security-group-ingress --
+    echo "Security group 'awesome-sg' created with id: $sg_id"
 fi
+
+# authorize security group ingress
+aws ec2 authorize-security-group-ingress --group-id "$sg_id" --protocol tcp --port 22 --cidr 0.0.0.0/0
