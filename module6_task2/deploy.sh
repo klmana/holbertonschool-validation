b448e0cbf6298a91837a6b5d008ca91e8c469cea
klmana <104742971+klmana@users.noreply.github.com>
2023-01-23 14:38:47 +1000

    update deploy

--- module6_task2/deploy.sh
+++ module6_task2/deploy.sh
@@ -1,18 +1,30 @@
 #!/bin/bash
 
-if [ -z "$1" ]; then
-    echo "Error: Please provide the path to the docker-compose file as an argument."
-    exit 1
+set -eux -o pipefail
+
+script_dir="$(cd "$(dirname "$0")" && pwd -P)"
+cd "${script_dir}/"
+
+test -n "${1}" || { echo "ERROR: please provide the relative path to a docker-compose file as argument"; exit 1; }
+
+## If a remote machine is used, then copy Prometheus's
+# configuration file on it
+compose_cmd=("docker-compose" "--file" "${1}")
+if test "${DOCKER_HOST:-notexisting}" != "notexisting"
+then
+  ssh_host="$(echo "${DOCKER_HOST}" | cut -d/ -f3,4)"
+  echo "== Remote Docker Engine detected: copying files to remote machine"
+  rsync -av "${script_dir}/" "${ssh_host}:~/"
+  compose_cmd=("ssh" "${ssh_host}" "${compose_cmd[@]}")
+else
+  echo "== Docker Engine is local: assuming local files"
 fi
 
-# Set the path to the docker-compose file
-docker_compose_path=$1
+## Cleanup previous containers and images if it already exists
+docker ps -q | xargs docker kill || true
+docker system prune --volumes --force
 
-# Set the remote hostname
-# DOCKER_HOST=ssh://ubuntu@$JENKINS_REMOTE_HOSTNAME
-
-# Deploy the Docker Compose file
-compose_cmd=("docker-compose" "-f" "$docker_compose_path")
+## Deploy with compose
 "${compose_cmd[@]}" pull
 "${compose_cmd[@]}" build
 "${compose_cmd[@]}" up --detach
